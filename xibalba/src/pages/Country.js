2ecc21672687fc92927e5d04e762481f6e1b2184
Kathya Castaneda <kathyakathya.kc@gmail.com>
2022-12-09 16:31:03 -0600

    created onclick function that navigates to individual cave

--- xibalba/src/pages/Country.js
+++ xibalba/src/pages/Country.js
@@ -1,28 +1,47 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Country = () => {
   let { id } = useParams()
+  let navigate = useNavigate()
   const [country, setCountry] = useState('')
 
   useEffect(() => {
     const getCountry = async () => {
       const response = await axios.get(`http://localhost:3001/countries/${id}`)
       setCountry(response.data)
-      console.log(' response data caves', response.data.Caves)
-      console.log('state caves', country.Caves)
-      console.log('underwater caves', country.Underwaters)
     }
     getCountry()
   }, [])
+
+  const viewCave = (caveId) => {
+    navigate(`/countries/${id}/cave/${caveId}`)
+  }
   return (
     <div>
       <h1>{country.name}</h1>
-      <h1>Caves</h1>
-      {country ? country.Caves.map((cave) => <h2>{cave.name}</h2>) : null}
-      <h1> Underwater Caves</h1>
-      {country ? country.Underwaters.map((cave) => <h2>{cave.name}</h2>) : null}
+      <div>
+        <h1>Caves</h1>
+        {country
+          ? country.Caves.map((cave) => (
+              <h2
+                onClick={() => {
+                  viewCave(cave.id)
+                }}
+              >
+                {cave.name}
+              </h2>
+            ))
+          : null}
+      </div>
+      <div>
+        <h1> Underwater Caves</h1>
+        {country
+          ? country.Underwaters.map((cave) => <h2>{cave.name}</h2>)
+          : null}
+      </div>
     </div>
   )
 }
