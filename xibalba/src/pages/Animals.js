e721dfc044aa60e864fe70f9acf18c8e2321ad9b
Kathya Castaneda <kathyakathya.kc@gmail.com>
2022-12-12 12:23:52 -0600

    form to create new cave done

--- xibalba/src/pages/Animals.js
+++ xibalba/src/pages/Animals.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Animals = () => {
   let navigate = useNavigate()
--- xibalba/src/pages/Profile.js
+++ xibalba/src/pages/Profile.js
@@ -1,7 +1,83 @@
+import { useState } from 'react'
+import axios from 'axios'
+
 const Profile = () => {
+  const initialState = {
+    name: '',
+    location: '',
+    details: '',
+    image: '',
+    countryId: ''
+  }
+
+  const [form, setForm] = useState(initialState)
+
+  const handleChange = (event) => {
+    setForm({
+      ...form,
+      [event.target.id]: event.target.value,
+      countryId: parseInt(event.target.value)
+    })
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    let addCave = await axios.post(`http://localhost:3001/caves`, form)
+    setForm(initialState)
+  }
+
   return (
     <div>
       <h2>Add your explorations here</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="cave_name">Name</label>
+          <input
+            type="text"
+            id="name"
+            defaultValue={form.name}
+            onChange={handleChange}
+          />
+          <label htmlFor="cave_location">Location</label>
+          <input
+            type="text"
+            id="location"
+            defaultValue={form.location}
+            onChange={handleChange}
+          />
+          <label htmlFor="cave_details">Details</label>
+          <input
+            type="text"
+            id="details"
+            defaultValue={form.details}
+            onChange={handleChange}
+          />
+          <label htmlFor="cave_image">Image</label>
+          <input
+            type="text"
+            id="image"
+            defaultValue={form.image}
+            onChange={handleChange}
+          />
+          <label htmlFor="country" type="number">
+            Country
+          </label>
+          <select
+            id="countryId"
+            defaultValue={form.countryId}
+            onChange={handleChange}
+          >
+            <option value="1">United States</option>
+            <option value="2">Mexico</option>
+            <option value="3">Guatemala</option>
+            <option value="4">New Zealand</option>
+            <option value="5">Scotland</option>
+            <option value="6">Malaysia</option>
+            <option value="7">Philippines</option>
+          </select>
+          <button type="submit">Send</button>
+        </div>
+      </form>
     </div>
   )
 }
